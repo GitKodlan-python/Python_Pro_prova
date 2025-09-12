e80623e65ecbeaf4df413ac0436f02684c94fa9a
anthony <anthony.bellari@gmail.com>
2025-09-12 18:29:57 +0200

    Update primo_file.py
    ho cambiato qualcosa

--- primo_file.py
+++ primo_file.py
@@ -2,3 +2,4 @@
 import random
 curiosita = ["il mio passa tempo sono i videogiochi", "il mio cane si chiama zoe", "il mio cibo preferito è la pizza", "sono simpatico"]
 print(random.choice(curiosita))
+waFETY
