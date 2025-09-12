e26f76605c56cfb1cc1711f7c38366d72ba3c4a3
GitKodlan-python <anthony.bellari@gmail.com>
2025-09-12 18:05:00 +0200

    Create primo_file.py
    codice primo file (liste)

---
+++ primo_file.py
@@ -0,0 +1,4 @@
+# lista sugli hobby
+import random
+curiosita = ["il mio passa tempo sono i videogiochi", "il mio cane si chiama zoe", "il mio cibo preferito è la pizza", "sono simpatico"]
+print(random.choice(curiosita))
