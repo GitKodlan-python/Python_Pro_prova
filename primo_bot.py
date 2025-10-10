a3963775555b8f8fac695b68f2c7dfae6bad31e3
anthony <anthony.bellari@gmail.com>
2025-10-10 17:10:54 +0200

    Update primo_bot.py
    ho avanzato il codice

--- primo_bot.py
+++ primo_bot.py
@@ -1,29 +1,46 @@
 import discord
+import random
+from discord.ext import commands
 
-# la variabile intents contiene i permessi al bot
 intents = discord.Intents.default()
-# abilita il permesso a leggere i contenuti dei messaggi
 intents.message_content = True
-# crea un bot e passa gli indents
-client = discord.Client(intents=intents)
 
-@client.event
+bot = commands.Bot(command_prefix='$', intents=intents)
+caratteri = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+@bot.event
 async def on_ready():
-    print(f'Abbiamo fatto l\'accesso come {client.user}')
+    print(f'Hai fatto l\'accesso come {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$ciao'):
-        await message.channel.send("Ciao!")
-    if message.content.startswith('$Io sono triste'):
-        await message.channel.send("Non mi interessa")
-    if message.content.startswith('$saluto'):
-        await message.channel.send("Ciao a tutti!")
-    elif message.content.startswith('$arrivederci'):
-        await message.channel.send("\U0001f642")
+@bot.command()
+async def ciao(ctx):
+    await ctx.send(f'Ciao! Sono un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+@bot.command()
+async def gen_emodji(ctx):
+    emodji = ["\U0001f600", "\U0001f642", "\U0001F606", "\U0001F923"]
+    await ctx.send(random.choice(emodji))
+
+@bot.command()
+async def password(ctx, count_password:int):
+    await ctx.send(random.choice(caratteri) * count_password)
+
+ 
+
+def flip_coin():
+    flip = random.randint(0, 2)
+    if flip == 0:
+        return "HEADS"
     else:
-        await message.channel.send(message.content)
+        return "TAILS"
 
-client.run("METTI IL TOKEN")
+bot.run("Metti il tuo token")
