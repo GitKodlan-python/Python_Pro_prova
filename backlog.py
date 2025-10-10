918a46feca284b9d4dd5288d4e8d9536841bfe1b
anthony <anthony.bellari@gmail.com>
2025-10-10 15:03:23 +0200

    Update backlog.py
    ho aggiunto la descrizione di un altro file

--- backlog.py
+++ backlog.py
@@ -1,4 +1,4 @@
 nel file "esercizio per casa.py" ho creato un codice che funzionava come dizionario se non sapevi le parole.
 nel file "password.py" ho creato un codice che mi generava un password casuale.
 nel file "primo_bot.py" ho creato un codice per il mio primo bot.
-nel file 
+nel file "lista_hobby.py ho creato un codice che mi generava una frase casuale tra quelle che ho scritto che riguardavano i miei hobby.
