ac04aa9dc9d34ec51eb24985b0b4169aeab9a741
anthony <anthony.bellari@gmail.com>
2025-10-10 14:57:11 +0200

    Update backlog.py
    ho aggiornato il mio backlog

--- backlog.py
+++ backlog.py
@@ -1,2 +1,4 @@
-nel file "esercizio per cassa" ho creato un codice che funzionava come dizionario se non sapevi le parole.
-nel file "password" ho creato
+nel file "esercizio per cassa.py" ho creato un codice che funzionava come dizionario se non sapevi le parole.
+nel file "password.py" ho creato un codice che mi generava un password casuale.
+nel file "primo_bot.py" ho creato un codice per il mio primo bot.
+nel file 
