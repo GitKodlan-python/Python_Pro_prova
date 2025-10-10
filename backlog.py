c698e4f41695f2d79910c0a524e0267a6842aaaa
anthony <anthony.bellari@gmail.com>
2025-10-10 15:00:04 +0200

    Update backlog.py
    ho aggiustato gli errori

--- backlog.py
+++ backlog.py
@@ -1,4 +1,4 @@
-nel file "esercizio per cassa.py" ho creato un codice che funzionava come dizionario se non sapevi le parole.
+nel file "esercizio per casa.py" ho creato un codice che funzionava come dizionario se non sapevi le parole.
 nel file "password.py" ho creato un codice che mi generava un password casuale.
 nel file "primo_bot.py" ho creato un codice per il mio primo bot.
 nel file 
