70d0b47621311b8bd72143d35a04843ca853889b
anthony <anthony.bellari@gmail.com>
2025-10-17 18:20:57 +0200

    Update backlog.py
    ho messo altre descrizioni di altri file

--- backlog.py
+++ backlog.py
@@ -3,3 +3,5 @@ nel file "password.py" ho creato un codice che mi generava un password casuale.
 nel file "primo_bot.py" ho creato un codice per il mio primo bot.
 nel file "lista_hobby.py ho creato un codice che mi generava una frase casuale tra quelle che ho scritto che riguardavano i miei hobby.
 nel file "text.py" ho creato un codice per il mio text.txt che ho su visaual studio code
+nel file "bot_inquinamento.py" ho messo lo stesso codice del file "primo_bot.py" perchè abbiamo fatto una lezione sull'inquinamento e poi ho aggiornato anche il file "primo_bot.py"
+nel file "text_inquinamento.py" ho creato un testo per l'inquinamento
