168984004c50d70a12dcf2c88200a4344cea0b68
anthony <anthony.bellari@gmail.com>
2025-10-17 18:12:43 +0200

    Implement new bot commands for memes and pollution
    ho aggiunto dei comandi

--- primo_bot.py
+++ primo_bot.py
@@ -51,10 +51,20 @@ async def scrivi(ctx):
         text = "ciao a tutti! oggi programmo il mio bot"
         f.write(text)
 
+@bot.command()
+async def inquinamento2(ctx):
+    with open('text_inquinamento.txt', 'r', encoding='utf-8') as f:
+        #print(f.read())
+        await ctx.send(f.read())
+
+@bot.command()
+async def inquinamento(ctx):
+    curiosita = ["l'inquinamento  non fa bene alla terra", "l'inquinamento che produciamo ci danneggia"]
+    await ctx.send(random.choice(curiosita))
 
 @bot.command()
 async def mem(ctx):
-    with open('images/mem1.jpg', 'rb') as f:
+    with open('meme/mem1.png', 'rb') as f:
         # Memorizziamo il file della libreria di Discord convertito in questa variabile!
         picture = discord.File(f)
    # Possiamo quindi inviare questo file come parametro!
@@ -62,9 +72,48 @@ async def mem(ctx):
 
 @bot.command()
 async def mem2(ctx):
-    img_name = random.choice(os.listdir('images'))
-    with open(f'images/{img_name}', 'rb') as f:
+    with open(f'meme/mem2.png', 'rb') as f:
             picture = discord.File(f)
     await ctx.send(file=picture)
+
+@bot.command()
+async def mem3(ctx):
+    with open(f'meme/mem3.png', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+@bot.command()
+async def mem4(ctx):
+    with open(f'meme/mem4.jpg', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+@bot.command()
+async def mem5(ctx):
+    with open(f'meme/mem5.jpg', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+@bot.command()
+async def mem6(ctx):
+    with open(f'meme/mem6.jpeg', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+@bot.command()
+async def mem7(ctx):
+    with open(f'meme/mem7.jpeg', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+@bot.command()
+async def mem8(ctx):
+    with open(f'meme/mem8.jpg', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+@bot.command()
+async def inquinamento_fa_male(ctx):
+    await ctx.send(f'si fa male, quindi NON FARE INQUINAMENTO!!!')
 
 bot.run("metti il token")
